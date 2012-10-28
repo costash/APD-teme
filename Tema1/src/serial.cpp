d3a24658bdd735cd2416790776aaa4d816f262c9
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-10-28 16:21:44 +0200

    serial.cpp checks for input args

--- Tema1/src/serial.cpp
+++ Tema1/src/serial.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -9,7 +10,8 @@ int main(int argc, char *argv[])
         cerr << argv[0] << " T file_in file_out\n";
         return 1;
     }
-    int iteratii
+    int iteratii = atoi(argv[1]);
+    cout << "Nr iteratii: " << iteratii << "\n";
 
     return 0;
 }
