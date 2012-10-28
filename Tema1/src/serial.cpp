b97af13ac46a9f4a99837cdb2ca63ab50efbe601
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-10-28 16:13:10 +0200

    Added serial.cpp

---
+++ Tema1/src/serial.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        cerr << "Usage error:\n";
+        cerr << argv[0] << " T file_in file_out\n";
+        return 1;
+    }
+    int iteratii
+
+    return 0;
+}
