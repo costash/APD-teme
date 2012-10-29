1fab9635587454a05b1b0d6a491123d1fcebb1aa
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-10-29 23:19:57 +0200

    Added brute computations

--- Tema1/src/serial.cpp
+++ Tema1/src/serial.cpp
@@ -1,8 +1,51 @@
 #include "common.h"
+#include <climits>
 using namespace std;
 
+// Gets the minimum price for own resource from (celli, cellj) perspective
+int getCostMinimResursaProprie(const bool resursa, Cell **& stats, const int n,
+		const int celli, const int cellj)
+{
+	int minim = INT_MAX;
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			if (stats[i][j].resursa == resursa &&
+					manhattan(celli, cellj, i, j) + stats[i][j].pret_resursa < minim)
+				minim = stats[i][j].pret_resursa;
+		}
+	return minim;
+}
 
+// Gets the minimum price for complementary resource from (celli, cellj) perspective
+int getCostMinimResursaCompl(const bool resursa, Cell **& stats, const int n,
+		const int celli, const int cellj)
+{
+	int minim = INT_MAX;
+	for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+			{
+				if (i == celli && j == cellj)
+					continue;
+				if (stats[i][j].resursa == resursa &&
+						manhattan(celli, cellj, i, j) + stats[i][j].pret_resursa < minim)
+					minim = stats[i][j].pret_resursa;
+			}
+		return minim;
+}
 
+// Computes and adds minimums to matrix
+void addMinCostToMatrix(const int n, Cell **& stats)
+{
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			stats[i][j].cost_compl = getCostMinimResursaCompl(!stats[i][j].resursa,
+					stats, n, i, j);
+			stats[i][j].cost_minim_resursa = getCostMinimResursaProprie(stats[i][j].resursa,
+					stats, n, i, j);
+		}
+}
 
 int main(int argc, char *argv[])
 {
@@ -21,7 +64,8 @@ int main(int argc, char *argv[])
 
     readInput(file_in, n, stats);
 
-    cerr << "manhattan (0,0) -> (3, 4) = " << manhattan(0, 0, 3, 4);
+    //cerr << "manhattan (0,0) -> (3, 4) = " << manhattan(0, 0, 3, 4);
+    addMinCostToMatrix(n, stats);
 
     return 0;
 }
