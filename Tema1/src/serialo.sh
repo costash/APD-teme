59834b3603b95eac4611e207c9c8535ae0b5b48e
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-11-04 17:21:25 +0200

    Serial optimized works

---
+++ Tema1/src/serialo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ANI=$1
+FISIN=$2
+FISOUT=$3
+
+time ./serialo $ANI $FISIN $FISOUT
--- Tema1/src/serialoptimized.cpp
+++ Tema1/src/serialoptimized.cpp
@@ -30,6 +30,70 @@ void printDebug(const int n, Cell **& mat)
 	}
 }
 
+void printCostMin(const int n, Cell **& stats)
+{
+	cout << "Costuri minime: \n";
+	for (int i = 0; i < n; ++i)
+	{
+		for (int j = 0; j < n; ++j)
+		{
+			cout << "(" << stats[i][j].cost_minim_resursa << ","
+					<< stats[i][j].cost_compl << ") ";
+		}
+		cout << endl;
+	}
+}
+
+// Writes
+void writeOutput(ofstream &file_out, const int n, Cell **& stats)
+{
+	int countResursaA = 0, countResursaB = 0;
+	int pretMaxA = 0, pretMaxB = 0;
+
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			if (!stats[i][j].resursa)
+			{
+				++countResursaA;
+				if (stats[i][j].pret_resursa > pretMaxA)
+					pretMaxA = stats[i][j].pret_resursa;
+			}
+			else
+			{
+				++countResursaB;
+				if (stats[i][j].pret_resursa > pretMaxB)
+					pretMaxB = stats[i][j].pret_resursa;
+			}
+		}
+	file_out << countResursaA << " " << pretMaxA << " ";
+	file_out << countResursaB << " " << pretMaxB << "\n";
+}
+
+// Write final costs.
+void writeCosts(const int n, Cell **& stats, ofstream &file_out)
+{
+	for (int i = 0; i < n; ++i)
+		{
+			for (int j = 0; j < n; ++j)
+				file_out << "(" << stats[i][j].resursa << ","
+					<< stats[i][j].pret_resursa << ","
+					<< stats[i][j].buget << ") ";
+			file_out << "\n";
+		}
+}
+
+inline int getMin2(const int a, const int b)
+{
+	return a > b ? b : a;
+}
+
+inline int getMin3(const int a, const int b, const int c)
+{
+	int aux = getMin2(a, b);
+	return getMin2(aux, c);
+}
+
 /**
  * Copy from a to b
  */
@@ -43,9 +107,9 @@ void copyMatrix(const int n, Cell **a, Cell **b)
 
 void computeMin(const int n, Cell **& stats, Cell **& aux)
 {
-	copyMatrix(n, stats, aux);
-	cout << "Before lines_left->right\n";
-	printDebug(n, stats);
+	/*cout << "Before lines_left->right\n";
+	printDebug(n, stats);*/
+	// Calculez minimele pe linii
 	for (int i = 0; i < n; ++i)
 	{
 
@@ -58,7 +122,7 @@ void computeMin(const int n, Cell **& stats, Cell **& aux)
 		{
 			bool res = stats[i][j - 1].resursa;
 			int pret = stats[i][j - 1].pret_resursa;
-			if (min[res] > pret + 1)
+			if (min[res] > pret)
 				min[res] = pret + 1;
 			else
 				++min[res];
@@ -83,7 +147,7 @@ void computeMin(const int n, Cell **& stats, Cell **& aux)
 		{
 			bool res = stats[i][j + 1].resursa;
 			int pret = stats[i][j + 1].pret_resursa;
-			if (min[res] > pret + 1)
+			if (min[res] > pret)
 				min[res] = pret + 1;
 			else
 				++min[res];
@@ -113,9 +177,190 @@ void computeMin(const int n, Cell **& stats, Cell **& aux)
 			}
 		}
 	}
-	cout << "After lines left->Right + right->left\n";
-	printDebug(n, stats);
+
+	// Salvez matricea obtinuta pentru a compara din nou cu ea la final
+	copyMatrix(n, stats, aux);
+
+	/*cout << "After lines left->Right + right->left\n";
+	printDebug(n, stats);*/
+
+	// Calculez minimele pe coloane, pe matricea obtinuta
+	for (int j = 0; j < n; ++j)
+	{
+		int min[2];
+		min[0] = stats[0][j].cost_minim_resursa;
+		min[1] = stats[0][j].cost_compl;
+		// Calculez minimele de sus in jos
+		for (int i = 1; i < n; ++i)
+		{
+			bool res = stats[i - 1][j].resursa;
+			int pret = stats[i - 1][j].pret_resursa;
+			if (min[res] > pret)
+				min[res] = pret + 1;
+			else
+				++min[res];
+			++min[!res];
+
+			if (res == false)		// resursa de tip A
+			{
+				if (stats[i][j].cost_minim_resursa > min[res])
+					stats[i][j].cost_minim_resursa = min[res];
+				else
+					min[res] = stats[i][j].cost_minim_resursa;
+				if (stats[i][j].cost_compl > min[!res])
+					stats[i][j].cost_compl = min[!res];
+				else
+					min[!res] = stats[i][j].cost_compl;
+			}
+			else					// resursa de tip B
+			{
+				if (stats[i][j].cost_compl > min[res])
+					stats[i][j].cost_compl = min[res];
+				else
+					min[res] = stats[i][j].cost_compl;
+				if (stats[i][j].cost_minim_resursa > min[!res])
+					stats[i][j].cost_minim_resursa = min[!res];
+				else
+					min[!res] = stats[i][j].cost_minim_resursa;
+			}
+		}
+
+		// Calculez minimele de jos in sus
+		min[0] = stats[n - 1][j].cost_minim_resursa;
+		min[1] = stats[n - 1][j].cost_compl;
+		for (int i = n - 2; i >= 0; --i)
+		{
+			bool res = stats[i + 1][j].resursa;
+			int pret = stats[i + 1][j].pret_resursa;
+			if (min[res] > pret)
+				min[res] = pret + 1;
+			else
+				++min[res];
+			++min[!res];
+
+			if (res == false)		// resursa de tip A
+			{
+				if (stats[i][j].cost_minim_resursa > min[res])
+					stats[i][j].cost_minim_resursa = min[res];
+				else
+					min[res] = stats[i][j].cost_minim_resursa;
+				if (stats[i][j].cost_compl > min[!res])
+					stats[i][j].cost_compl = min[!res];
+				else
+					min[!res] = stats[i][j].cost_compl;
+			}
+			else					// resursa de tip B
+			{
+				if (stats[i][j].cost_compl > min[res])
+					stats[i][j].cost_compl = min[res];
+				else
+					min[res] = stats[i][j].cost_compl;
+				if (stats[i][j].cost_minim_resursa > min[!res])
+					stats[i][j].cost_minim_resursa = min[!res];
+				else
+					min[!res] = stats[i][j].cost_minim_resursa;
+			}
+		}
+
+	}
+
+	/*cout << "After columns up->down + down->up\n";
+	printDebug(n, stats);*/
+
+	// Compar ce am obtinut cu minimele pe linii
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			bool res = stats[i][j].resursa;
+			if (res == 0)
+			{
+				stats[i][j].cost_minim_resursa = getMin3(stats[i][j].cost_minim_resursa,
+						aux[i][j].cost_minim_resursa, stats[i][j].pret_resursa);
+				stats[i][j].cost_compl = getMin2(stats[i][j].cost_compl,
+						aux[i][j].cost_compl);
+			}
+			else
+			{
+				stats[i][j].cost_minim_resursa = getMin2(stats[i][j].cost_minim_resursa,
+						aux[i][j].cost_minim_resursa);
+				stats[i][j].cost_compl = getMin3(stats[i][j].cost_compl,
+						aux[i][j].cost_compl, stats[i][j].pret_resursa);
+			}
+			/*stats[i][j].cost_minim_resursa = getMin3(stats[i][j].cost_minim_resursa,
+					aux[i][j].cost_minim_resursa, stats[i][j].pret_resursa);
+			stats[i][j].cost_compl = getMin3(stats[i][j].cost_compl,
+					aux[i][j].cost_compl, stats[i][j].pret_resursa);*/
+		}
+
+	/*cout << "Final min\n";
+	printDebug(n, stats);*/
+
 }
+
+void computeNextYear(const int n, Cell **stats, Cell **aux)
+{
+	computeMin(n, stats, aux);
+	//printCostMin(n, stats);
+	for (int i = 0; i < n; ++i)
+		for (int j = 0; j < n; ++j)
+		{
+			int cost_compl, cost_min_res;
+			bool res = stats[i][j].resursa;
+			// Get minimum
+			if (res == 0)
+			{
+				cost_compl = stats[i][j].cost_compl;
+				cost_min_res = stats[i][j].cost_minim_resursa;
+			}
+			else
+			{
+				cost_compl = stats[i][j].cost_minim_resursa;
+				cost_min_res = stats[i][j].cost_compl;
+			}
+
+			// Actualizare
+			if (stats[i][j].buget < cost_compl)
+			{
+				stats[i][j].pret_resursa += cost_compl - stats[i][j].buget;
+			}
+			else if (stats[i][j].buget > cost_compl)
+			{
+				stats[i][j].pret_resursa += (cost_compl - stats[i][j].buget) / 2;
+			}
+			else
+			{
+				stats[i][j].pret_resursa = cost_min_res + 1;
+			}
+			stats[i][j].buget = cost_compl;
+
+			if (stats[i][j].pret_resursa < pret_minim)
+				stats[i][j].pret_resursa = pret_minim;
+			else if (stats[i][j].pret_resursa > pret_maxim)
+			{
+				// respecializare
+				stats[i][j].resursa = !stats[i][j].resursa;
+				stats[i][j].buget = pret_maxim;
+				stats[i][j].pret_resursa = (pret_minim + pret_maxim) / 2;
+			}
+		}
+}
+
+// Computes requested data for all years
+void computeAllYears(const int n, Cell **stats, Cell **aux, ofstream &file_out)
+{
+	for (int k = 0; k < iteratii; ++k)
+	{
+		/*cout << "Pasul " << k << "\n";*/
+		computeNextYear(n, stats, aux);
+
+		writeOutput(file_out, n, stats);
+
+		/*cout << "\n";
+		printCosts(n, stats);*/
+	}
+	writeCosts(n, stats, file_out);
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -147,8 +392,9 @@ int main(int argc, char *argv[])
     printCosts(n, aux);*/
 
     //computeAllYears(n, stats, file_out);
-    computeMin(n, stats, aux);
-
+    //computeMin(n, stats, aux);
+    //computeNextYear(n, stats, aux);
+    computeAllYears(n, stats, aux, file_out);
 
     file_out.close();
     return 0;
