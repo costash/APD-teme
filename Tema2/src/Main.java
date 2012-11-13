673867adfe37487ae11614de01d68e51b23ebcd3
Constantin Serban-Radoi <costashsrc@gmail.com>
2012-11-13 13:13:20 +0200

    Added Tema2

---
+++ Tema2/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Constantin Șerban-Rădoi 333CA
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test\n");
+	}
+
+}
